d2b2feb610ce2ed852583b0369e1a1d01055c161
Yuurij-dev <yurijesus.dev@gmail.com>
2025-05-17 00:42:21 -0300

    First Commit

---
+++ script.js
@@ -0,0 +1,57 @@
+let qr;
+
+function generateQRCode() {
+  const url = document.getElementById("urlInput").value;
+  const qrContainer = document.getElementById("qrcode");
+  const areaInput = document.getElementById('area-input')
+  const areaCode = document.getElementById("area-code")
+
+  const erroInput = document.getElementById('erro-input')
+
+  if (!url) {
+    alert("Por favor, insira uma URL.");
+    erroInput.style.visibility = 'visible'
+    return;
+  }
+
+  // Limpa QR antigo
+  areaInput.style.right = '2000px' 
+  areaCode.style.left = 0
+  qrContainer.style.display = "block";
+  qrContainer.innerHTML = "";
+
+  // Cria novo QR Code
+  qr = new QRCode(qrContainer, {
+    text: url,
+    width: 200,
+    height: 200,
+    colorDark: "#111629",   
+  });
+}
+
+function downloadQRCode() {
+  const img = document.querySelector("#qrcode img");
+
+  if (!img) {
+    alert("Por favor, gere um QR Code primeiro.");
+    return;
+  }
+
+  const link = document.createElement("a");
+  link.href = img.src;
+  link.download = "qrcode.png";
+  link.click();
+}
+
+function copyToClipboard() {
+  const url = document.getElementById("urlInput").value;
+
+  if (!url) {
+    alert("Por favor, insira uma URLdasd.");
+    return;
+  }
+
+  navigator.clipboard.writeText(url)
+    .then(() => alert("URL copiada para a área de transferência!"))
+    .catch(() => alert("Falha ao copiar."));
+}
